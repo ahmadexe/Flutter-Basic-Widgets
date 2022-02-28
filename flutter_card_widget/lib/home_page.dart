1bfa8d1d5e7b4a5679c62af937111cd11dad2416
Ahmad <muahmad710@gmail.com>
2022-03-01 03:56:20 +0500

    Updating ScrollView

--- flutter_card_widget/lib/home_page.dart
+++ flutter_card_widget/lib/home_page.dart
@@ -203,9 +203,7 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
           ],
-        )
-
-
+        ),
       ]),
     );
   }
---
+++ flutter_scrollview/lib/home_page.dart
@@ -0,0 +1,301 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 80,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Container(
+                    height: 180,
+                    width: 140,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(2.0, 2.5),
+                            blurRadius: 6,
+                          )
+                        ]),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 200,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(2.0, 2.5),
+                      blurRadius: 6,
+                    )
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
